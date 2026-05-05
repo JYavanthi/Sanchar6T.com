34d7ccdf5c05e235111950c9ab3e664463585022
Rushikesh Rodage <rushikesh.r@techvaraha.com>
2026-05-05 12:16:04 +0530

    fic cross error

--- Sanchar6T/src/config/axiosInstance.ts
+++ Sanchar6T/src/config/axiosInstance.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+    baseURL: "http://198.38.91.204:5001",
   headers: {
     "Content-Type": "application/json",
   },
