7e76be1fe24a61948248be23adb072e4fc389d2c
Rushikesh Rodage <rushikesh.r@techvaraha.com>
2026-05-05 14:43:43 +0530

    chnage base url for bitla

--- Sanchar6T/src/API-URLS.tsx
+++ Sanchar6T/src/API-URLS.tsx
@@ -2,5 +2,5 @@
 export const API_URLS = {
   //  API_BASE : "http://localhost:5000/api"
   // API_BASE_URL: "http://198.38.91.204:5001/api"
- API_BASE_URL: "https://sanchar6t.com"
+ API_BASE_URL: "https://sanchar6t.com/api"
 };
