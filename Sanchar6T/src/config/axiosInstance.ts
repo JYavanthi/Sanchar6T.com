c676a0136286a120bed59a7894d9fafe49ac6dd6
Rushikesh Rodage <rushikesh.r@techvaraha.com>
2026-05-05 14:23:12 +0530

    chnage base url for bitla

--- Sanchar6T/src/config/axiosInstance.ts
+++ Sanchar6T/src/config/axiosInstance.ts
@@ -2,7 +2,8 @@ import axios from "axios";
 
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://sanchar6t.com",
   headers: {
     "Content-Type": "application/json",
   },
