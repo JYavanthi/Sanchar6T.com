32dce390e6e6933db42fd4f88f56a4d0976d541d
Rushikesh Rodage <rushikesh.r@techvaraha.com>
2026-05-05 14:56:10 +0530

    backend env

--- Sanchar6T-Backend/modules/bitla/bitla.router.js
+++ Sanchar6T-Backend/modules/bitla/bitla.router.js
@@ -4,7 +4,12 @@ import BitlaController from "./bitla.controller.js";
 const router = express.Router();
 const controller = new BitlaController();
 
-router.get("/cities", controller.getCities);
+// router.get("/cities", controller.getCities);
+
+router.get("/cities", (req, res) => {
+  console.log("🔥 BITLA ROUTE HIT");
+  controller.getCities(req, res);
+});
 router.get("/city-pairs", controller.getCityPairs);
 router.get("/paired-cities/:cityId", controller.getPairedCities);
 router.get("/schedules/:originId/:destinationId/:date", controller.getSchedules);
